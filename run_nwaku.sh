e3a6946cb5b541d7ad0a4d49b024da9346b014df
Gabriel mermelstein <gabrielmermelstein@gmail.com>
2024-03-11 12:53:12 -0600

    deprecating rpc

--- run_nwaku.sh
+++ run_nwaku.sh
@@ -14,7 +14,7 @@ echo "NODE_INDEX $NODE_INDEX"
 RETRIES=${RETRIES:=10}
 
 while [ -z "${BOOTSTRAP_ENR}" ] && [ ${RETRIES} -ge 0 ]; do
-  BOOTSTRAP_ENR=$(wget -O - --post-data='{"jsonrpc":"2.0","method":"get_waku_v2_debug_v1_info","params":[],"id":1}' --header='Content-Type:application/json' http://bootstrap:8545/ 2> /dev/null | sed 's/.*"enrUri":"\([^"]*\)".*/\1/');
+  BOOTSTRAP_ENR=$(wget -qO- http://bootstrap:8645/debug/v1/info --header='Content-Type:application/json' 2> /dev/null | sed 's/.*"enrUri":"\([^"]*\)".*/\1/');
   echo "Bootstrap node not ready, retrying (retries left: ${RETRIES})"
   sleep 1
   RETRIES=$(( $RETRIES - 1 ))
@@ -28,9 +28,7 @@ fi
 echo "Using bootstrap node: ${BOOTSTRAP_ENR}"
 exec /usr/bin/wakunode\
       --relay=true\
-      --rpc-admin=true\
       --max-connections=250\
-      --rpc-address=0.0.0.0\
       --rest=true\
       --rest-admin=true\
       --rest-private=true\
@@ -42,7 +40,6 @@ exec /usr/bin/wakunode\
       --discv5-discovery=true\
       --discv5-enr-auto-update=True\
       --log-level=INFO\
-      --rpc-address=0.0.0.0\
       --metrics-server=True\
       --metrics-server-address=0.0.0.0\
       --discv5-bootstrap-node=${BOOTSTRAP_ENR}\
