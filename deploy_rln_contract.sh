bcffbdad3cdfc959fcd0aa5661ef63969e60c392
stubbsta <tanya.steyn22@gmail.com>
2024-04-24 14:56:58 +0200

    Update deploy-contract-script for new RLNv2 contract on different branch

--- deploy_rln_contract.sh
+++ deploy_rln_contract.sh
@@ -2,12 +2,19 @@
 
 set -e
 
+which yarn
+echo $PATH
+export PATH=$PATH:$HOME/.yarn/bin
+echo $PATH
+
 # 1. Install foundry
 curl -L https://foundry.paradigm.xyz | bash && . /root/.bashrc && foundryup && export PATH=$PATH:$HOME/.foundry/bin
 
 #. 2. Clone and build waku-rln-contract repo
 git clone https://github.com/waku-org/waku-rln-contract.git
 cd /waku-rln-contract
+git checkout rln-v2
+
 forge install && yarn install && yarn compile
 
 # 3. Create .env file with RPC_PROVIDER variable
