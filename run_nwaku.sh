2e22d801e5169d540583e7c3c9b7922dfa28e04f
stubbsta <tanya.steyn22@gmail.com>
2024-05-15 09:57:25 +0200

    add pubsub-topic and cluster-id to run_nwaku.sh

--- run_nwaku.sh
+++ run_nwaku.sh
@@ -91,4 +91,6 @@ exec /usr/bin/wakunode\
       --metrics-server=True\
       --metrics-server-address=0.0.0.0\
       --discv5-bootstrap-node=${BOOTSTRAP_ENR}\
-      --nat=extip:${IP}
+      --nat=extip:${IP}\
+      --pubsub-topic=/waku/2/rs/66/0\
+      --cluster-id=66
