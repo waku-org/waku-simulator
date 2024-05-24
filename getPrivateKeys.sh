00e8d65ba8461a16d4d5057940d03ca5e0629897
stubbsta <tanya.steyn22@gmail.com>
2024-05-24 10:01:36 +0200

    read keys from foundry config and write to shared file

---
+++ getPrivateKeys.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Install json parser
+apk update
+apk add jq
+
+# Read the JSON file
+json_content=$(cat anvil-config.txt)
+
+# Extract private_keys array values using jq
+private_keys=$(echo "$json_content" | jq -r '.private_keys[]')
+
+# Write private keys to a new file for easier access
+echo "Writing private keys to file"
+echo "$private_keys" > /shared/private-keys.txt
