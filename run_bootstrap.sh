dc27f4c661dc0ecd352bdf2bf26c355a4226a035
Tanya S <120410716+stubbsta@users.noreply.github.com>
2024-07-01 12:09:37 +0200

    Simplify bootstrap node config (#80)
    * simplify bootstrap node config, only discv5
    
    * remove keep-alive from bootstrap conf

--- run_bootstrap.sh
+++ run_bootstrap.sh
@@ -5,12 +5,11 @@ IP=$(ip a | grep "inet " | grep -Fv 127.0.0.1 | sed 's/.*inet \([^/]*\).*/\1/')
 echo "I am a bootstrap node"
 
 exec /usr/bin/wakunode\
-      --relay=true\
+      --relay=false\
       --rest=true\
       --rest-admin=true\
       --rest-private=true\
       --rest-address=0.0.0.0\
-      --keep-alive=true\
       --max-connections=300\
       --dns-discovery=true\
       --discv5-discovery=true\
@@ -20,9 +19,5 @@ exec /usr/bin/wakunode\
       --metrics-server-address=0.0.0.0\
       --nodekey=30348dd51465150e04a5d9d932c72864c8967f806cce60b5d26afeca1e77eb68\
       --nat=extip:${IP}\
-      --rest=true\
-      --rest-admin=true\
-      --rest-private=true\
-      --rest-address=0.0.0.0\
       --pubsub-topic=/waku/2/rs/66/0\
       --cluster-id=66
